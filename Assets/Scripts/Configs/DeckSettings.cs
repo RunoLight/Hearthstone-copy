4f858cbbb2621c891bcf61d8dc6fadb124502fa4
Vasily Kuznetsov <31756050+RunoLight@users.noreply.github.com>
2022-01-24 18:01:16 +1000

    Deck config description

--- Assets/Scripts/Configs/DeckSettings.cs
+++ Assets/Scripts/Configs/DeckSettings.cs
@@ -5,19 +5,21 @@ namespace Configs
     [CreateAssetMenu(fileName = "Deck Settings", menuName = "Deck Settings")]
     public class DeckSettings : ScriptableObject
     {
+        [Header("Cards amount when generated")]
         public int minimalCardAmount = 4;
         public int maximalCardAmount = 6;
-        
+        [Space(15)]
         public PlayingCard cardPrefab;
-        
+        [Header("Space between cards in hand")]        
         public float gapFromOneItemToTheNextOne = 1;
-
+        [Header("Total degree twist of hand cards")]
         public float totalTwist = 20f;
+        [Header("Bend value for arc where card positioned at")]
         public float scalingFactor = 0.01f;
-
+        [Header("Hover selection")]
         public Vector3 scaleCardWhenSelected = new Vector3(1.2f, 1.2f, 1f);
         public float scaleCardDuration = 0.2f;
-        
+        [Header("Deselection")]
         public Vector3 scaleBack = Vector3.one;
         public float scaleBackDuration = 0.2f;
     }
